2959a3f3e87d89e2fd593612b94f931a639f7b9a
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-29 01:37:44 +0200

    changed viewSet for borrowing, added createserializer

--- borrowings/views.py
+++ borrowings/views.py
@@ -4,12 +4,14 @@ from borrowings.models import Borrowing
 from borrowings.serializers import (
     BorrowingSerializer,
     BorrowingDetailSerializer,
+    BorrowingCreateSerializer
 )
 
 
 class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
     viewsets.GenericViewSet
 ):
     queryset = Borrowing.objects.all()
@@ -18,4 +20,9 @@ class BorrowingViewSet(
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowingDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
