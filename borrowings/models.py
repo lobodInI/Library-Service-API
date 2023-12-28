65bc6e34c3fee98a34231ad57d696e7065db5289
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-28 23:21:35 +0200

    Added constraints for borrow_date, expected_return_date, and actual_return_date.

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Q, CheckConstraint, F
 
 from books.models import Book
 
@@ -18,3 +21,25 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings",
     )
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                check=Q(borrow_date__gte=datetime.now().date()),
+                name="borrow_date_gte_or_equal_today"
+            ),
+            CheckConstraint(
+                check=Q(expected_return_date__gte=F("borrow_date")),
+                name="expected_return_after_borrow"
+            ),
+            CheckConstraint(
+                check=Q(
+                    actual_return_date__gte=F("borrow_date"),
+                    actual_return_date__lte=F("expected_return_date")
+                ),
+                name="actual_return_between_borrow_and_expected"
+            ),
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.user} borrowed {self.book.title}"
