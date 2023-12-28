db8edcaa9058848167908c5c26049969fe254948
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-28 18:07:13 +0200

    added DEFAULT_PERMISSION_CLASSES for REST_FRAMEWORK in settings.py

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -130,7 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "books.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ],
 }
 
 SIMPLE_JWT = {
