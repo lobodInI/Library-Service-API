4dfb2b329670cd40cb1b4f233c18f600a08039e6
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-29 12:57:43 +0200

    added condition: non-admins can see only their borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,5 +1,9 @@
+from typing import Type
+
+from django.db.models import QuerySet
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.serializers import Serializer
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
@@ -19,12 +23,22 @@ class BorrowingViewSet(
     serializer_class = BorrowingSerializer
     permission_classes = (IsAuthenticated, )
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Type[Serializer]:
         if self.action == "retrieve":
             return BorrowingDetailSerializer
         if self.action == "create":
             return BorrowingCreateSerializer
         return self.serializer_class
+
+    def get_queryset(self) -> QuerySet:
+        return self.filter_queryset(self.queryset)
+
+    def filter_queryset(self, queryset) -> QuerySet:
+        user = self.request.user
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
