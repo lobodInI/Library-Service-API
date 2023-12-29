bdd1b12fa48056d1ca05481ed986e788e86414bb
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-29 18:41:53 +0200

    added spectacular settings

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "drf_spectacular",
     "rest_framework",
+    "drf_spectacular",
     "books",
     "users",
     "borrowings"
